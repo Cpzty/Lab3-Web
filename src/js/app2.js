3e4cb6deadeb195eb9d2aadc5bac2e7faff450dc
Cpzty <Cristianpzty@gmail.com>
2018-07-18 19:18:25 -0600

    obteniendo datos del json

--- src/js/app2.js
+++ src/js/app2.js
@@ -1,13 +1,15 @@
+const block = document.createElement('div');
+block.className = "block";
+
 const solicitud =fetch('https://raw.githubusercontent.com/samuelchvez/todos-fake-json-api/master/db.json');
 solicitud
 .then(resultado =>  resultado.json())
-.then(resultadoJSON => console.log(Object.keys(resultadoJSON).length))
+.then(resultadoJSON => block.innerHTML=resultadoJSON[0].title,root.appendChild(block) )
+
 .catch(error => console.log("error", error));
 
-const jlength=Object.keys(solicitud).length;
-console.log(jlength);
-const block = document.createElement('div');
-block.className = "block";
-root.appendChild(block);
+
+
+
 
  
